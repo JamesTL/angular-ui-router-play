f3f9ecff191e7c173832da823e4f2fff9fa33782
jameslove <hello@jameslove.co.uk>
2015-07-02 18:02:44 +0100

    Adding basic page sidplay authentication functionality

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,14 +1,28 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('angularUiRouterPlay')
-    .run(runBlock);
+    angular
+        .module('angularUiRouterPlay')
+        .run(runBlock);
 
-  /** @ngInject */
-  function runBlock($log) {
+    /** @ngInject */
+    function runBlock($log,$rootScope) {
 
-    $log.debug('runBlock end');
-  }
+        $rootScope.currentUser;
+
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+            console.log('data', toState);
+
+
+            var requireLogin = toState.data.requireLogIn;
+
+            if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+                event.preventDefault();
+                // get me a login modal!
+            }
+        });
+
+        $log.debug('runBlock end');
+    }
 
 })();
