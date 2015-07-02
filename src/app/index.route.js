266cfa6011ea194321541109c106c0dd3fa21a17
jameslove <hello@jameslove.co.uk>
2015-07-02 18:01:36 +0100

    Refactoring state definitons into name objects - added data value for authentication

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,93 +7,102 @@
 
     /** @ngInject */
     function routeConfig($stateProvider, $urlRouterProvider) {
+
+        var home = {
+            name: 'home',
+            url: '/',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'main'
+        };
+//ABOUT
+        var about = {
+            name: 'about',
+            url: '/about',
+            views: {
+                '': {templateUrl: 'app/about/about.html'},
+                'right-col@about': {template: "right col about"}
+            }
+        };
+//PROFILE
+        var profile = {
+
+            name: 'profile',
+            url: '/profile',
+            data: {
+
+                requireLogIn: true
+            },
+            views: {
+                '': {templateUrl: 'app/profile/profile.html'},
+                'right-col@profile': {
+                    template: 'right col profile'
+                }
+            }
+        };
+        var profiletimeline = {
+            name: 'profile.timeline',
+            url: '/timeline',
+
+            views: {
+
+                'profile-main@profile': {
+                    template: 'Timeline content here'
+                }
+            }
+
+        };
+        var profileHandhistory = {
+            name: 'profile.handhistory',
+            url: '/handhistory',
+            views: {
+
+                'profile-main@profile': {
+                    template: 'Handhistory feed content here'
+                }
+            }
+        };
+        var profileNotifications = {
+            name: 'profile.notifications',
+            url: '/notifications',
+            views: {
+
+                'profile-main@profile': {
+                    template: 'Notifications feed content here'
+                }
+            }
+        };
+        var profileFollowing = {
+            name: 'profile.following',
+            url: '/following',
+            views: {
+
+                'profile-main@profile': {
+                    template: 'Following feed content here'
+                }
+            }
+        };
+        var profileFollowers = {
+            name: 'profile.followers',
+            url: '/followers',
+            views: {
+
+                'profile-main@profile': {
+                    template: 'Followers feed content here'
+                }
+            }
+
+        };
+
         $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'app/main/main.html',
-                controller: 'MainController',
-                controllerAs: 'main'
-            });
-        $stateProvider
-            .state('about', {
-                url: '/about',
-                views: {
-                    '': {templateUrl: 'app/about/about.html'},
-                    'right-col@about': {template: "right col about"}
-                }
-
-            });
-
-
-
-        $stateProvider.
-            state('profile', {
-
-                url: '/profile',
-                views: {
-                    '': {templateUrl: 'app/profile/profile.html'},
-                    'right-col@profile': {
-                        template: 'right col profile'
-                    }
-                }
-
-            })
-            .state('profile.timeline', {
-
-                url: '/profile/timeline',
-
-                views: {
-
-                    'profile-main@profile': {
-                        template: 'Timeline content here'
-                    }
-                }
-
-            })
-            .state('profile.handhistory', {
-
-                url:'/profile/handhistory',
-                views: {
-
-                    'profile-main@profile': {
-                        template: 'Handhistory feed content here'
-                    }
-                }
-
-            })
-            .state('profile.notifications', {
-
-                url:'/profile/notifications',
-                views: {
-
-                    'profile-main@profile': {
-                        template: 'Notifications feed content here'
-                    }
-                }
-
-            })
-            .state('profile.following', {
-
-                url:'/profile/following',
-                views: {
-
-                    'profile-main@profile': {
-                        template: 'Following feed content here'
-                    }
-                }
-
-            })
-            .state('profile.followers', {
-
-                url:'/profile/followers',
-                views: {
-
-                    'profile-main@profile': {
-                        template: 'Followers feed content here'
-                    }
-                }
-
-            });
+            .state(home)
+            .state(about)
+            .state(profile)
+            .state(profiletimeline)
+            .state(profileNotifications)
+            .state(profileHandhistory)
+            .state(profileFollowing)
+            .state(profileFollowers);
 
         $urlRouterProvider.otherwise('/');
     }
